847efa59d3141ca044489ab50e5f0e60c3acf727
Thomas Pope <tpope0928@gmail.com>
2019-01-02 21:44:29 +0000

    Automatically backed up by Learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -63,12 +63,12 @@ describe "Pokemon" do
     #xit "alters Pikachu's hp to 59" do
       #pikachu.alter_hp(59, @db)
       #expect(Pokemon.find(1, @db).hp).to eq(59)
-    end
+    #end
 
     # Now we alter Magikarp's hp
     #xit "alters Magikarp's hp" do
       #magikarp.alter_hp(0, @db)
       #expect(Pokemon.find(2, @db).hp).to eq(0)
-    end
+    #end
   end
 end
